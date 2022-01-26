8132b86f81cded5d70e5dee328063873550952a3
seojinoh <seojinoh.me@gmail.com>
2022-01-26 23:39:36 +0900

    feat : Spring Boot framework 프로젝트 추가

---
+++ src/main/java/me/seojinoh/learning/guava/LearningGuavaApplication.java
@@ -0,0 +1,13 @@
+package me.seojinoh.learning.guava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningGuavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningGuavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/seojinoh/learning/guava/LearningGuavaApplicationTests.java
@@ -0,0 +1,13 @@
+package me.seojinoh.learning.guava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningGuavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
