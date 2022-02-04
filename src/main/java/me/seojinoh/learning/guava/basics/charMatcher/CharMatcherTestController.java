63c757d2022afb87e75ad79b48bc0ad0adeb84bc
Seojin Oh <seojinoh.me@gmail.com>
2022-02-04 23:28:38 +0900

    feat : CharMatcher 클래스로 String에서 특수문자를 제거 추가

---
+++ src/main/java/me/seojinoh/learning/guava/basics/charMatcher/CharMatcherTestController.java
@@ -0,0 +1,38 @@
+package me.seojinoh.learning.guava.basics.charMatcher;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.google.common.base.CharMatcher;
+
+@RestController
+@RequestMapping(value = "/charMatcher")
+public class CharMatcherTestController {
+
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	/**
+	 * String에서 특수문자를 제거 (CharMatcher.retainFrom(CharSequence sequence) 활용)
+	 * <pre>
+	 * CharMatcher 클래스는 Java char value의 true or false를 결정하며, retainFrom 메서드는 문자 시퀀스의 일치하는 모든 BMP 문자를 포함한 문자열을 순서대로 반환합니다.
+	 * </pre>
+	 * @deprecated 대신, CharMatcher.inrange(char startInclusive, char endInclusive) 등의 사용을 권장합니다.
+	 */
+	@GetMapping("/removeSpecialCharacters")
+	public void removeSpecialCharacters() {
+		String input = "Hello, World! 12345";
+
+		CharMatcher charMatcher = CharMatcher.javaLetterOrDigit();
+		logger.info("Retain Letter and Digit ::: {}", charMatcher.retainFrom(input));
+
+		charMatcher = CharMatcher.javaLetter();
+		logger.info("Retain Letter ::: {}", charMatcher.retainFrom(input));
+
+		charMatcher = CharMatcher.digit();
+		logger.info("Retain Digit ::: {}", charMatcher.retainFrom(input));
+	}
+
+}
