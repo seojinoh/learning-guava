719eca1b49de64e61fa5b754e92c2db4c758372d
Seojin Oh <seojinoh.me@gmail.com>
2022-02-05 08:48:04 +0900

    docs : CharMatcher 주석 업데이트

--- src/main/java/me/seojinoh/learning/guava/basics/charMatcher/CharMatcherTestController.java
+++ src/main/java/me/seojinoh/learning/guava/basics/charMatcher/CharMatcherTestController.java
@@ -20,8 +20,8 @@ public class CharMatcherTestController {
 	 * CharMatcher 클래스는 Java char value의 true or false를 결정하며, retainFrom 메서드는 문자 시퀀스의 일치하는 모든 BMP 문자를 포함한 문자열을 순서대로 반환합니다.
 	 * </pre>
 	 * @deprecated 대신, CharMatcher.inrange(char startInclusive, char endInclusive) 등의 사용을 권장합니다.
-	 * CharMatcher의 메서드 중 "Deprecated. ... are supplementary characters;"를 포함한 docs와 함께 deprecated 된 메서드들이 있는데,
-	 * Devanagari, Fullwidth digits과 같은 이상한 문자와 일치될 수 있는 오해의 소지가 있기 때문입니다. (따라서, 실제 사용하여도 큰 이슈가 없을 수 있음)
+	 * CharMatcher의 메서드 중 "Deprecated. ... are supplementary characters;"를 포함한 javadoc과 함께 deprecated 된 메서드들이 있는데,
+	 * Devanagari, Fullwidth digits와 같은 의도하지 않은 문자와 일치될 수 있기 때문입니다. (따라서, 상황에 따라 실제 사용에 이슈가 없을 수 있음)
 	 */
 	@GetMapping("/removeSpecialCharacters")
 	public void removeSpecialCharacters() {
